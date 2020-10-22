5f32d84ebacf05d871f1bd4c2ac428a4fdb8ae1b
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-10-21 20:57:38 -0500

    inicio de botones

--- src/otroEjercicio/GuiRios.java
+++ src/otroEjercicio/GuiRios.java
@@ -6,45 +6,60 @@ import java.awt.event.*;
 
 public class GuiRios extends JFrame {
 
-    JComboBox TemporadaLluvias;
-    JButton Registrar, Municipio, MR, DP;
-    JTextField Nombre, Codigo, KilometrosExtencion, CantidadAfluentes;
+	JComboBox TemporadaLluvias;
+	JButton Registrar, Municipio, visulizarInformacionRios, mostrarCantidadRios,porcentajeRios;
+	JTextField codigo, Nombre, kilometrosDeExtension, CantidadAfluentes;
 
-    public GuiRios() {
-        setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
-        add(new JLabel("Nombre:"));
-        Nombre = new JTextField(20);
-        add(Nombre);
-        add(new JLabel("Codigo: "));
-        Codigo = new JTextField(20);
-        add(Codigo);
-        add(new JLabel("Temperatura Promedio: "));
-        KilometrosExtencion = new JTextField(20);
-        add(KilometrosExtencion);
-        add(new JLabel("Temperatura Promedio: "));
-        CantidadAfluentes = new JTextField(20);
-        add(CantidadAfluentes);
+	public GuiRios() {
+		setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
 
-        add(new JLabel("Alerta Maxima: "));
-        TemporadaLluvias = new JComboBox();
-        TemporadaLluvias.addItem("Amarilla");
-        TemporadaLluvias.addItem("Naranja");
-        TemporadaLluvias.addItem("Roja");
-        add(TemporadaLluvias);
-        Registrar = new JButton("Registrar");
-        add(Registrar);
-        //guardar.addActionListener(new boton1());
-        Municipio = new JButton("Municipios");
-        add(Municipio);
+		add(new JLabel("Codigo:"));
+		codigo = new JTextField(20);
+		add(codigo);
 
-        setSize(300, 400);
-        setVisible(true);
-        setTitle("Rios");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
+		add(new JLabel("Nombre:"));
+		Nombre = new JTextField(20);
+		add(Nombre);
+		
+		add(new JLabel("Cantidad de afluentes: "));
+		CantidadAfluentes = new JTextField(20);
+		add(CantidadAfluentes);
 
-    public static void main(String[] args) {
-        GuiRios ventana = new GuiRios();
-    }
+		add(new JLabel("Kilometros de extensión: "));
+		kilometrosDeExtension = new JTextField(20);
+		add(kilometrosDeExtension);
+
+		add(new JLabel("Alerta Maxima: "));
+		TemporadaLluvias = new JComboBox();
+		TemporadaLluvias.addItem("Amarilla");
+		TemporadaLluvias.addItem("Naranja");
+		TemporadaLluvias.addItem("Roja");
+		add(TemporadaLluvias);
+
+		Registrar = new JButton("Registrar");
+		add(Registrar);
+		// guardar.addActionListener(new boton1());
+
+		mostrarCantidadRios = new JButton("Cantidad de Rios");
+		add(mostrarCantidadRios);
+		// guardar.addActionListener(new boton2());
+
+		visulizarInformacionRios = new JButton("Mostrar informacion de rios");
+		add(visulizarInformacionRios);
+		// guardar.addActionListener(new boton3());
+
+		porcentajeRios = new JButton("Porcentaje Rios con más ");
+		add(porcentajeRios);
+		// guardar.addActionListener(new boton4());
+
+		setSize(300, 500);
+		setVisible(true); 
+		setTitle("Rios");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		GuiRios ventana = new GuiRios();
+	}
 
 }

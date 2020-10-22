3210dc330b5f8f0a3fff127bb5d6a11743a4647c
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-10-21 20:06:16 -0500

    se copia el archivo de controlador Municipio como ejemplo

---
+++ src/otroEjercicio/ControladorMunicipio.java
@@ -0,0 +1,42 @@
+package otroEjercicio;
+
+public class ControladorMunicipio {
+
+	Municipio muni[];
+	int cont;
+
+	public ControladorMunicipio(int n) {
+		muni = new Municipio[n];
+		cont = 0;
+
+	}
+
+	public void anadirMunicipio(String nombre, double distanciaDesdeMedellinKm, double temperaturaPromedio, String zona) {
+		if (cont < muni.length) {
+			muni[cont] = new Municipio(nombre, distanciaDesdeMedellinKm, temperaturaPromedio, zona);
+			cont++;
+		}
+	}
+
+	public Municipio[] getMunicipio() {
+		return muni;
+	}
+
+	public double distanciaPromedio() {
+		double distanciaPromedio = 0;
+
+		for (int i = 0; i < cont; i++) {
+			distanciaPromedio += muni[i].getDistanciaDesdeMedellinKm();
+		}
+
+		distanciaPromedio /= cont;
+
+		return distanciaPromedio;
+
+	}
+
+	public int getCont() {
+		return cont;
+	}
+
+}
